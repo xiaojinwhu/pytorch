6515562a759929135e779c5c5cdcfd2dd0b3d6a0
Peter Bell <peterbell10@live.co.uk>
2022-05-30 21:19:12 +0100

    ATen/native (3/6): Use per-operator headers
    ghstack-source-id: 33f81abe04a1219ad21663821b4c373a994afb74
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75573

--- aten/src/ATen/native/ConvolutionMM2d.cpp
+++ aten/src/ATen/native/ConvolutionMM2d.cpp
@@ -1,5 +1,6 @@
 #define TORCH_ASSERT_ONLY_METHOD_OPERATORS
 #include <ATen/core/Tensor.h>
+#include <ATen/native/ConvUtils.h>
 #include <ATen/Dispatch.h>
 #include <ATen/Parallel.h>
 #include <ATen/TensorUtils.h>
